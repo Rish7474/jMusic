aba7aa46d335b49ca168f16726dc803bb9bab3f1
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2019-05-30 17:52:55 -0500

    Black Keys added

--- keyboard.java
+++ keyboard.java
@@ -12,12 +12,11 @@ import java.awt.event.KeyEvent;
 
 public class keyboard extends JPanel implements KeyListener{
    private ArrayList<String> notes = new ArrayList<String>(Arrays.asList("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"));
-   private boolean[] keysDown = new boolean[8];
+   private boolean[] keysDown = new boolean[13];
    private  MidiChannel[] channels;
    private int instrument = 1; 
    private  int volume = 150;
    private int octave = 3;
-   private int x = 0;
    public keyboard(){
       addKeyListener(this);
       setFocusable(true);
@@ -34,6 +33,7 @@ public class keyboard extends JPanel implements KeyListener{
    
    public void paint(Graphics g){
       drawWhiteKeys(g);
+      drawBlackKeys(g);
    }
    
    public void increaseVol(){volume++;}
@@ -53,13 +53,27 @@ public class keyboard extends JPanel implements KeyListener{
    
    public void drawWhiteKeys(Graphics g){
       g.setColor(Color.white);
-      int itr = 0; //iterative number
+      int itr = 0; //key number
       for(int c = 5; c<800;c+=100){
          if(keysDown[itr])
             g.setColor(Color.yellow);
          g.fillRect(c,0,95,500);
          g.setColor(Color.white);
          itr++;
+      }
+   }
+   
+   public void drawBlackKeys(Graphics g){
+      g.setColor(Color.black);
+      int itr = 8;
+      for(int c = 75; c<700;c+=100){     
+         if(keysDown[itr])
+            g.setColor(Color.red);
+       g.fillRect(c,0,50,200);
+       g.setColor(Color.black);
+       if(itr==9 || itr==11)
+         c+=100;
+       itr++;
       }
    }
    
@@ -113,18 +127,23 @@ public class keyboard extends JPanel implements KeyListener{
             break;
          case KeyEvent.VK_Q:
             channels[instrument].noteOn(id("G#"),volume);
+            keysDown[8] = true;
             break;
          case KeyEvent.VK_W:
             channels[instrument].noteOn(id("A#"),volume);
+            keysDown[9] = true;
             break; 
          case KeyEvent.VK_R:
             channels[instrument].noteOn(id("C#"),volume);
+            keysDown[10] = true;
             break; 
          case KeyEvent.VK_T:
             channels[instrument].noteOn(id("D#"),volume);
+            keysDown[11] = true;
             break;  
          case KeyEvent.VK_U:
             channels[instrument].noteOn(id("F#"),volume);
+            keysDown[12] = true;
             break;
          case KeyEvent.VK_1:
             instrument = 1;
@@ -132,8 +151,9 @@ public class keyboard extends JPanel implements KeyListener{
          case KeyEvent.VK_2:
             instrument = 9;
             break;
-    }      
+     }      
    }
+   
    @Override
    public void keyTyped(KeyEvent e){}
    @Override
@@ -141,38 +161,44 @@ public class keyboard extends JPanel implements KeyListener{
     int keyCode = e.getKeyCode();
       switch (keyCode){ 
          case KeyEvent.VK_A:
-            channels[instrument].noteOn(id("G",0),volume);
             keysDown[0] = false;
             break;     
          case KeyEvent.VK_S:
-            channels[instrument].noteOn(id("A",0),volume);
             keysDown[1] = false;
             break;  
          case KeyEvent.VK_D:
-            channels[instrument].noteOn(id("B",0),volume);
             keysDown[2] = false;
             break; 
          case KeyEvent.VK_F:
-            channels[instrument].noteOn(id("C"),volume);
             keysDown[3] = false;
             break;    
          case KeyEvent.VK_G:
-            channels[instrument].noteOn(id("D"),volume);
             keysDown[4] = false;
             break;  
          case KeyEvent.VK_H:
-            channels[instrument].noteOn(id("E"),volume);
             keysDown[5] = false;
             break;
          case KeyEvent.VK_J:
-            channels[instrument].noteOn(id("F"),volume);
             keysDown[6] = false;
             break;
          case KeyEvent.VK_K:
-            channels[instrument].noteOn(id("G"),volume);
             keysDown[7] = false;
             break;
+         case KeyEvent.VK_Q:
+            keysDown[8] = false;
+            break;
+         case KeyEvent.VK_W:
+            keysDown[9] = false;
+            break; 
+         case KeyEvent.VK_R:
+            keysDown[10] = false;
+            break; 
+         case KeyEvent.VK_T:
+            keysDown[11] = false;
+            break;  
+         case KeyEvent.VK_U:
+            keysDown[12] = false;
+            break;
        }
-
    }
 }

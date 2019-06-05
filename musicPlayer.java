cdd3dcd94dd26a5220c17256095b79a73c6cea11
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2019-06-05 18:50:16 -0500

    Update musicPlayer.java

--- musicPlayer.java
+++ musicPlayer.java
@@ -1,12 +1,10 @@
-import java.awt.Color; 
-import javax.swing.JFrame;
+ import javax.swing.JFrame;
 import javax.swing.JPanel; 
 public class musicPlayer{
    public static void main(String args[]){
       JFrame frame = new JFrame();
       frame.setBounds(150,50,800,500);
       frame.setTitle("Sound Keyboard");
-      frame.setBackground(Color.blue);
       frame.setResizable(true);
       keyboard k = new keyboard();
       frame.add(k);
